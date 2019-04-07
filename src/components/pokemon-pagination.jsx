cbc2069cb749eafa111ec43c580157bbcca94e48
Raymundo Garay <r1a2y3@gmail.com>
2019-04-07 13:28:36 -0500

    refactor: changing identation in pokemon-pagination

--- src/components/pokemon-pagination.jsx
+++ src/components/pokemon-pagination.jsx
@@ -2,64 +2,57 @@ import React, { Component } from "react";
 import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
 
 function renderPaginationNumbers({ actualPage, lastPage, getPokemonList }) {
-    const listPagination = [];
-    let prevPage = 0;
-    let postPage = actualPage + 1;
+  const listPagination = [];
+  let prevPage = 0;
+  let postPage = actualPage + 1;
 
-    if (actualPage > 0) {
-      prevPage = actualPage - 1;
-    }
-
-    listPagination.push(
-      <PaginationItem key="pagination-first">
-        <PaginationLink first onClick={() => getPokemonList()} />
-      </PaginationItem>
-    );
-
-    listPagination.push(
-      <PaginationItem key="pagination-previous">
-        <PaginationLink previous onClick={() => getPokemonList(prevPage)} />
-      </PaginationItem>
-    );
-
-    for (
-      let i = actualPage + 1;
-      i < actualPage + 6 && i <= lastPage;
-      i++
-    ) {
-      listPagination.push(
-        <PaginationItem key={"pagination-" + i}>
-          <PaginationLink onClick={() => getPokemonList(i - 1)}>
-            {i}
-          </PaginationLink>
-        </PaginationItem>
-      );
-    }
-
-    if (postPage > lastPage) {
-      postPage = lastPage;
-    }
-
-    listPagination.push(
-      <PaginationItem key="pagination-next">
-        <PaginationLink next onClick={() => getPokemonList(postPage)} />
-      </PaginationItem>
-    );
-    listPagination.push(
-      <PaginationItem key="pagination-last">
-        <PaginationLink
-          last
-          onClick={() => getPokemonList(lastPage)}
-        />
-      </PaginationItem>
-    );
-
-    return listPagination;
+  if (actualPage > 0) {
+    prevPage = actualPage - 1;
   }
+
+  listPagination.push(
+    <PaginationItem key="pagination-first">
+      <PaginationLink first onClick={() => getPokemonList()} />
+    </PaginationItem>
+  );
+
+  listPagination.push(
+    <PaginationItem key="pagination-previous">
+      <PaginationLink previous onClick={() => getPokemonList(prevPage)} />
+    </PaginationItem>
+  );
+
+  for (let i = actualPage + 1; i < actualPage + 6 && i <= lastPage; i++) {
+    listPagination.push(
+      <PaginationItem key={"pagination-" + i}>
+        <PaginationLink onClick={() => getPokemonList(i - 1)}>
+          {i}
+        </PaginationLink>
+      </PaginationItem>
+    );
+  }
+
+  if (postPage > lastPage) {
+    postPage = lastPage;
+  }
+
+  listPagination.push(
+    <PaginationItem key="pagination-next">
+      <PaginationLink next onClick={() => getPokemonList(postPage)} />
+    </PaginationItem>
+  );
+  listPagination.push(
+    <PaginationItem key="pagination-last">
+      <PaginationLink last onClick={() => getPokemonList(lastPage)} />
+    </PaginationItem>
+  );
+
+  return listPagination;
+}
 
 export default function PaginationPokemon(props) {
   const paginationToShow = props.shouldShowPagination ? (
-      <Pagination>{renderPaginationNumbers(props)}</Pagination>
-    ) : null;
-    return paginationToShow;
+    <Pagination>{renderPaginationNumbers(props)}</Pagination>
+  ) : null;
+  return paginationToShow;
 }
