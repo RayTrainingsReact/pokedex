5fa713698398eb325bf7b24e0c1440e2b15a78a5
Raymundo Garay <r1a2y3@gmail.com>
2019-04-07 13:54:37 -0500

    refactor: changing pokemon-info to functional component

--- src/components/pokemon-info.jsx
+++ src/components/pokemon-info.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import pokeball from "./../pokeball.png";
 import {
   Card,
@@ -10,100 +10,99 @@ import {
   Progress
 } from "reactstrap";
 
-class PokemonInfo extends Component {
-  renderInfo() {
-    return (
-      <Container>
-        <Card>
-          <CardTitle className="text-center">
-            #{this.props.info.order} - {this.props.info.name}{" "}
-          </CardTitle>
-          <CardBody>
-            <Row>
-              <Col>
-                <img
-                  src={this.props.info.frontDefault}
-                  alt={this.props.info.name + " image"}
-                />
-              </Col>
-              <Col>
-                {this.props.fightInfo.map(particularInfo => {
-                  return (
-                    <Row key={particularInfo.label}>
-                      <Col>{particularInfo.label}</Col>
-                      <Col>
-                        <Progress
-                          striped
-                          color="success"
-                          value={particularInfo.value}
-                        >
-                          {particularInfo.value}%
-                        </Progress>
-                      </Col>
-                    </Row>
-                  );
-                })}
-              </Col>
-            </Row>
-          </CardBody>
-        </Card>
+function renderInfo(props) {
+  return (
+    <Container>
+      <Card>
+        <CardTitle className="text-center">
+          <h1>
+            #{props.info.order} - {props.info.name}
+          </h1>
+        </CardTitle>
+        <CardBody>
+          <Row>
+            <Col>
+              <img
+                src={props.info.frontDefault}
+                alt={props.info.name + " image"}
+              />
+            </Col>
+            <Col>
+              {props.fightInfo.map(particularInfo => {
+                return (
+                  <Row key={particularInfo.label}>
+                    <Col>{particularInfo.label}</Col>
+                    <Col>
+                      <Progress
+                        striped
+                        color="success"
+                        value={particularInfo.value}
+                      >
+                        {particularInfo.value}%
+                      </Progress>
+                    </Col>
+                  </Row>
+                );
+              })}
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
 
-        <Card>
-          <CardBody>
-            <Row>
-              <Col>
-                <b>Height:</b> {this.props.info.height}
-              </Col>
-              <Col>
-                <b>Weight:</b> {this.props.info.weight}
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <b>Catch Rate:</b> {this.props.pokemonSpecies.catchRate}
-              </Col>
-              <Col>
-                <b>Gender Ratio:</b> {this.props.pokemonSpecies.genderRatio}
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <b>Egg Groups:</b> {this.props.pokemonSpecies.eggGroups}
-              </Col>
-              <Col>
-                <b>Hatch Steps:</b> {this.props.pokemonSpecies.hatchSteps}
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <b>Abilities:</b> {this.props.info.abilities}
-              </Col>
-              <Col>
-                <b>EVs:</b> 1 Sp Att
-              </Col>
-            </Row>
-          </CardBody>
-        </Card>
-      </Container>
-    );
-  }
-
-  renderEmptyState() {
-    return (
-      <Container className="text-center">
-        <h1>Empty State</h1>
-        <img src={pokeball} alt="Empty State Pokeball" />
-      </Container>
-    );
-  }
-
-  render() {
-    const pokemonInfo =
-      this.props.pokemonSelected >= 0
-        ? this.renderInfo()
-        : this.renderEmptyState();
-    return pokemonInfo;
-  }
+      <Card>
+        <CardTitle>
+          <h2>Profile</h2>
+        </CardTitle>
+        <CardBody>
+          <Row>
+            <Col>
+              <b>Height:</b> {props.info.height}
+            </Col>
+            <Col>
+              <b>Weight:</b> {props.info.weight}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <b>Catch Rate:</b> {props.pokemonSpecies.catchRate}
+            </Col>
+            <Col>
+              <b>Gender Ratio:</b> {props.pokemonSpecies.genderRatio}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <b>Egg Groups:</b> {props.pokemonSpecies.eggGroups}
+            </Col>
+            <Col>
+              <b>Hatch Steps:</b> {props.pokemonSpecies.hatchSteps}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <b>Abilities:</b> {props.info.abilities}
+            </Col>
+            <Col>
+              <b>EVs:</b> 1 Sp Att
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
+    </Container>
+  );
 }
 
-export default PokemonInfo;
+function renderEmptyState(props) {
+  return (
+    <Container className="text-center">
+      <h1>Empty State</h1>
+      <img src={pokeball} alt="Empty State Pokeball" />
+    </Container>
+  );
+}
+
+export default function PokemonInfo(props) {
+  const pokemonInfo =
+    props.pokemonSelected >= 0 ? renderInfo(props) : renderEmptyState();
+  return pokemonInfo;
+}
